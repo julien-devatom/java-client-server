d6979bdd5f9dded9055d69d4a6c841e6d34e70a0
julien <julien.thomas84@gmail.com>
2021-10-19 20:43:44 +0200

    Improve Port & IP validation

--- src/com/devatom/client/Client.java
+++ src/com/devatom/client/Client.java
@@ -1,72 +1,77 @@
 package com.devatom.client;
 
-import com.devatom.server.InvalidCommandExecutionException;
-
 import java.io.*;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
 
 public class Client {
 
-    public static String HOST = "127.0.0.1";
-    public static int PORT = 5005;
+    public static String HOST = "";
+    public static int PORT = 0;
     private  DataOutputStream os;
     private  DataInputStream is;
     private static final BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
     private  Socket connection;
 
-    public static boolean validate(final String ip) {
+    public static boolean isIPInvalid() {
         // IPv4 pattern
         String PATTERN = "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$";
-        return ip.matches(PATTERN);
+        return !Client.HOST.matches(PATTERN);
     }
-    public static void EntrerAdresseEtPort() throws IOException {
-        System.out.format("SVP entrer l'adresse IP et le port avec un enter entre chaque info\n");
-        Client.HOST = consoleReader.readLine();
-        Client.PORT = Integer.parseInt(consoleReader.readLine());
 
-        while(Client.PORT<5002 || Client.PORT>5049)
-        {
-            System.out.format("SVP entrer un port entre 5002 et 5049\n");
-            Client.PORT = Integer.parseInt(consoleReader.readLine());
-        }
-        while(!validate(Client.HOST))
-        {
-            System.out.format("SVP entrer un addresse IP valide XXX.XXX.XXX.XXX\n");
-            Client.HOST = consoleReader.readLine();
-        }
-    }
-    public Client() {
+    public Client() throws IOException {
         try{
-            Client.EntrerAdresseEtPort();
+            SetupServerAddressAndPort();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
+        System.out.println("Connected to the server " + connection.toString());
+    }
+    private void SetupServerAddressAndPort() throws IOException {
+        while (Client.isIPInvalid()){
+            System.out.println("Please enter the IP address of the server (XXX.XXX.XXX.XXX)");
+            System.out.print("IP address = ");
+            Client.HOST = consoleReader.readLine();
+            if(Client.isIPInvalid())
+                System.out.println(Client.HOST + " is not a valid IPv4...");
+        }
+
+        while(Client.PORT<5002 || Client.PORT>5049)
+        {
+            System.out.println("Please select the port of the server (between 5002 et 5049) ");
+            try{
+                System.out.print("Port = ");
+                Client.PORT = Integer.parseInt(consoleReader.readLine());
+                if(Client.PORT<5002 || Client.PORT>5049)
+                    System.out.println(Client.PORT + " is not a valid port");
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number..");
+            }
+        }
+        serverConnection();
+    }
+    private void serverConnection() throws IOException {
         // on se connecte au serveur
         try {
+            System.out.println("Connection to the server " + Client.HOST + ":" + Client.PORT + " ...");
             connection = new Socket(Client.HOST, Client.PORT);
             // on initialise les connections avec le serveur
             os = new DataOutputStream(connection.getOutputStream());
             is = new DataInputStream(connection.getInputStream());
-        } catch (ConnectException e)
-        {
+        } catch (ConnectException | UnknownHostException e) {
             System.out.println("Server" + Client.HOST + ":" + Client.PORT + " Unreachable");
-            System.exit(1);
-        }
-        catch (IOException e) {
+            SetupServerAddressAndPort();
+            } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Connected to the server " + connection.toString());
     }
 
     public void listen() throws IOException {
-        System.out.println("Enter a command");
         String inputCommand = consoleReader.readLine();
         try{
             confirmCommand(inputCommand);
@@ -80,25 +85,33 @@ public class Client {
             upload(inputCommand);
         if (Objects.equals(inputCommand.split(" ")[0], "download"))
             download(inputCommand);
+        if (Objects.equals(inputCommand.split(" ")[0], "disconnect")) {
+            disconnect();
+            return;
+        }
 
         String response;
         try {
             while (!Objects.equals(response = is.readUTF(), "EOF")) {
                 System.out.println(response);
                 if (response.equals("bye")) {
-                    connection.close();
-                    System.out.println("Disconnected");
+                    disconnect();
                     return;
                 }
             }
         } catch (EOFException e){
-            System.out.println("Server disconnected..");
-            connection.close();
+            disconnect();
             return;
         }
         listen();
     }
-
+    private void disconnect() throws IOException {
+        connection.close();
+        System.out.println("Server disconnected");
+        Client.HOST = "";
+        Client.PORT = 0;
+        SetupServerAddressAndPort();
+    }
     private void confirmCommand(String inputCommand) throws InvalidClientExecutionException {
         String command = inputCommand.split(" ")[0];
         if (Objects.equals(command, "download") & inputCommand.split(" ").length < 2){
